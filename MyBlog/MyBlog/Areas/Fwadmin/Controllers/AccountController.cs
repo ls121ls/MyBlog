021dc2bcda7ef967ae70075f4c4c048ba656e7f3
ls121ls <314888192@qq.com>
2016-12-20 13:33:25 +0800

    1

--- MyBlog/MyBlog/Areas/Fwadmin/Controllers/AccountController.cs
+++ MyBlog/MyBlog/Areas/Fwadmin/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using MyBlog.BLL;
+using MyBlog.Model;
 
 namespace MyBlog.Areas.Fwadmin.Controllers
 {
@@ -23,9 +25,14 @@ namespace MyBlog.Areas.Fwadmin.Controllers
         [HttpPost]
         public ActionResult Login(FormCollection collection)
         {
-            if (Request["name"] == "admin" && Request["password"] == "admin")
+            Account account = new Account();
+            TryUpdateModel(account);
+            ServiceRepository service = new ServiceRepository();
+            if (
+             service.AccountBLL.GetEntities(
+                 d => d.Name == account.Name && d.Password == account.Password).Count == 1)
             {
-                HttpContext.Session.Add("CurrentUser","admin");
+                HttpContext.Session.Add("CurrentAccount","admin");
                 HttpContext.Response.Cookies.Add(new HttpCookie("user","admin"));
                 return RedirectToAction("Index", "Home");
             }
--- MyBlog/MyBlog/Areas/Fwadmin/Controllers/AdminBaseController.cs
+++ MyBlog/MyBlog/Areas/Fwadmin/Controllers/AdminBaseController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using Common;
 using MyBlog.BLL;
 using MyBlog.Model;
@@ -17,15 +18,19 @@ namespace MyBlog.Areas.Fwadmin.Controllers
 
         public AdminBaseController()
         {
+        }
 
-            if (System.Web.HttpContext.Current.User.Identity.IsAuthenticated == false)
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if (HttpContext.Session["CurrentAccount"] == null)
             {
-                System.Web.HttpContext.Current.Response.Redirect("/Account/Login");
+                System.Web.HttpContext.Current.Response.Redirect("/Fwadmin/Account/Login");
                 System.Web.HttpContext.Current.Response.End();
             }
             else
             {
-                Account account = service.AccountBLL.GetUserByLoginName(System.Web.HttpContext.Current.User.Identity.Name);
+                Account account = service.AccountBLL.GetUserByLoginName(Session["CurrentAccount"].ToString());
                 CallContext.SetData("Account", account);
             }
             if (System.Web.HttpContext.Current.Request.Cookies["username"] == null)
@@ -39,7 +44,6 @@ namespace MyBlog.Areas.Fwadmin.Controllers
                 System.Web.HttpContext.Current.Response.Cookies["username"].Expires = DateTime.Now.AddDays(0.5);
             }
         }
-
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             //
